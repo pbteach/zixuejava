4852ca38f842a27e254f5e2babbf7a96e5433eef
pbteach <pbteach@126.com>
2019-07-08 08:09:02 +0800

    提交第一个程序

--- first_program/HelloWorld.java
+++ first_program/HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World 攀博网www.pbteach.com!");
+		System.out.println("Hello World 攀博网www.pbteach.com !");
 	}
 }
