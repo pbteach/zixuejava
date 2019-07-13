4bd8ba74a20d96c82fbd96b19980bcf4db88f146
pbteach <pbteach@126.com>
2019-07-13 16:54:51 +0800

    提交javadoc的代码

---
+++ javadoc/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+* 这个类是第一个Java程序
+* 功能：输出Hello World!
+* @author pbteach.com
+* @version 1.0
+*/
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		//输出HelloWorld
+		/*输出HelloWorld攀博网www.pbteach.com*/
+		System.out.println("Hello World 攀博网www.pbteach.com!");
+	}
+}
